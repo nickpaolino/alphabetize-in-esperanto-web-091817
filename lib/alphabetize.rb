cf310c93456c9b5371df51554f9fda91c6e0558f
github username <github email address>
2017-09-14 20:51:27 -0400

    Finished with the alphabetize method and passed the tests

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require "pry"
+
 def alphabetize(arr)
-  # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
+  arr.sort do |phrase_a, phrase_b|
+    phrase_a_sorted = phrase_a.delete(" ")
+    phrase_b_sorted= phrase_b.delete(" ")
+    a_array = phrase_a_sorted.split("").collect {|char| alphabet.index(char)}
+    b_array = phrase_b_sorted.split("").collect {|char| alphabet.index(char)}
+    a_array <=> b_array
+  end
 end
